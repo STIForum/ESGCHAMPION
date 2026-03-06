1be3f685caedaa4736bb0f65d42a69ed0cdabd6c
Thai son nguyen <150060408+Sondrdr2022@users.noreply.github.com>
2026-03-07 04:04:19 +0700

    Add files via upload

---
+++ sql-migrations/add missing champion detail.sql
@@ -0,0 +1,4 @@
+ALTER TABLE public.champions ADD COLUMN competence_esg text;
+ALTER TABLE public.champions ADD COLUMN sectors_focus text;
+ALTER TABLE public.champions ADD COLUMN expertise_panels text;
+ALTER TABLE public.champions ADD COLUMN website text;
